43581feaee5394ec94ae034bcf73a373f6d193d3
Martin Stensgård <mastensg@mastensg.net>
2016-11-02 03:18:45 +0100

    test-profile-arcs: march=core2
    Maybe Travis will build it now.

--- test-profile-arcs.sh
+++ test-profile-arcs.sh
@@ -5,20 +5,20 @@ set -e
 autoreconf -f -i
 
 echo >&2 "Baseline"
-./configure CFLAGS='-O3 -march=native' CC=gcc >/dev/null
+./configure CFLAGS='-O3 -march=core2' CC=gcc >/dev/null
 make clean >/dev/null
 make >/dev/null
 ./ray-headless
 
 
 echo >&2 "Profiling"
-./configure CFLAGS='-O3 -march=native -fprofile-generate' CC=gcc >/dev/null
+./configure CFLAGS='-O3 -march=core2 -fprofile-generate' CC=gcc >/dev/null
 make clean >/dev/null
 make >/dev/null
 ./ray-headless
 
 echo >&2 "Using profile"
-./configure CFLAGS='-O3 -march=native -fprofile-use -fprofile-correction' CC=gcc >/dev/null
+./configure CFLAGS='-O3 -march=core2 -fprofile-use -fprofile-correction' CC=gcc >/dev/null
 make clean >/dev/null
 make >/dev/null
 ./ray-headless
